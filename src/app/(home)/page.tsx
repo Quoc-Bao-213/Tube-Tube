8918caae8b9c44ff0ab96ce40a6522b1ed82d09e
Bao.Ta <bao.ta@s3corp.com.vn>
2025-10-12 14:31:52 +0700

    feat: integrate tRPC with Upstash Redis and implement rate limiting

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
+import { HydrateClient, trpc } from "@/trpc/server";
+import { PageClient } from "./client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+export default async function Home() {
+  void trpc.hello.prefetch({ text: "from tRPC" });
+
   return (
     <div>
-      <p className="text-center text-lg">Welcome to Next.js!</p>
+      <HydrateClient>
+        <Suspense fallback={<div>Loading</div>}>
+          <ErrorBoundary fallback={<div>Error</div>}>
+            <PageClient />
+          </ErrorBoundary>
+        </Suspense>
+      </HydrateClient>
     </div>
   );
 }
