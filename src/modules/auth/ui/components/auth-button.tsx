a7b7ed2deaa02f8a3d7509fde490f4c69a61b1e4
Bao.Ta <bao.ta@s3corp.com.vn>
2025-11-05 00:11:23 +0700

    feat: implement video view page with sections for video details, comments, and suggestions

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -23,7 +23,7 @@ export const AuthButton = () => {
         <SignInButton mode="modal">
           <Button
             variant="outline"
-            className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20 rounded-full shadow-none"
+            className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20 rounded-full shadow-none cursor-pointer"
           >
             <UserCircleIcon />
             Sign In
