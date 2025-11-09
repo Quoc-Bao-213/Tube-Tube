1c7452bdd0792f84a0434c1d7af363cf7f664c0d
Bao.Ta <bao.ta@s3corp.com.vn>
2025-11-09 13:59:27 +0700

    feat: implement comments feature with CRUD operations, UI components, and integration into video view

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -5,14 +5,16 @@ import { createTRPCRouter } from "../init";
 import { videoViewsRouter } from "@/modules/video-views/server/procedures";
 import { videoReactionsRouter } from "@/modules/video-reactions/server/procedures";
 import { subscriptionsRouter } from "@/modules/subscriptions/server/procedures";
+import { commentsRouter } from "@/modules/comments/server/procedures";
 
 export const appRouter = createTRPCRouter({
-  categories: categoriesRouter,
   studio: studioRouter,
   videos: videosRouter,
+  comments: commentsRouter,
+  categories: categoriesRouter,
   videoViews: videoViewsRouter,
-  videoReactions: videoReactionsRouter,
   subscriptions: subscriptionsRouter,
+  videoReactions: videoReactionsRouter,
 });
 
 export type AppRouter = typeof appRouter;
